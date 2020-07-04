9125955ca58b6da9b5f22bfd835caed8ed12accf
Eduardo Baranowski <eduardobaranowski@gmail.com>
2020-07-04 16:42:57 -0300

    logs

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,28 @@
-import 'reflect-metadata';
-import 'dotenv/config';
+import "reflect-metadata";
+import "dotenv/config";
 
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
-import './database';
-import routes from './routes';
+import "./database";
+import routes from "./routes";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+function logRequests(req, res, next) {
+  const { method, url } = req;
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+  console.time(logLabel);
+  next();
+  console.timeEnd(logLabel);
+}
+
+app.use(logRequests);
+
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
-  console.log('Server running');
+  console.log("Server running");
 });
